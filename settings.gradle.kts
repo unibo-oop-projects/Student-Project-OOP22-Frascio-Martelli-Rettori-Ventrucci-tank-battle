a80aa89ec61f3c765d931a87bea50efb0e17c715
Roberto Casadei <robyonrails@gmail.com>
2023-04-17 17:39:02 +0200

    oop machinery

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -8,3 +8,15 @@
  */
 
 rootProject.name = "TankBattle-project"
+
+plugins {
+    id("com.gradle.enterprise") version "3.13"
+}
+
+gradleEnterprise {
+    buildScan {
+        termsOfServiceUrl = "https://gradle.com/terms-of-service"
+        termsOfServiceAgree = "yes"
+        publishAlways()
+    }
+}
